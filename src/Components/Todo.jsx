fe4af7e7ad4eb444a1fc3d50c2183e5d55c65027
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2023-08-09 11:20:37 +0530

    first commit

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import {useState} from "react";
+import "./Todo.css";
+import TodoList from "./TodoList/TodoList";
+
+export default function Todo() {
+	let [task, setTask] = useState([]);
+	let [input, setInput] = useState("");
+
+	function submitData(event) {
+		event.preventDefault();
+		task.push(input);
+		setInput("");
+	}
+
+	function showChange(event) {
+		setInput(event.target.value);
+	}
+
+	return (
+		<div>
+			<div className="container">
+				<div className="todo-container">
+					<form action="" className="todo-form" onSubmit={submitData}>
+						<h1 className="todo-name">Todo App</h1>
+						<input
+							type="text"
+							className="todo-input"
+							placeholder="Enter your Task....."
+							onChange={showChange}
+							value={input}
+						/>
+					</form>
+				</div>
+				<div className="todo-list-container">
+					<TodoList task={task} setTask={setTask} />
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/Components/TodoList/TodoList.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import "./TodoList.css";
+import {FaTrash} from "react-icons/fa6";
+import {FaPencil} from "react-icons/fa6";
+
+export default function TodoList(props) {
+	const task = props.task;
+	const setTask = props.setTask;
+
+	function editTask(data) {
+		let newVal = prompt("Enter your new task")
+		let updatedTask = task.map((val)=>{
+			if(val === data){
+				return newVal
+			}else{
+				return val
+			}
+		})
+		setTask(updatedTask)
+	}
+
+	function deleteTask(data) {
+		let updatedTask = task.filter((val) => {
+			if (val != data) {
+				return val;
+			}
+		});
+		setTask(updatedTask);
+	}
+
+	let taskList = task.map((val) => {
+		return (
+			<li key={val.index} className="todo-list-li">
+				<div className="list">
+					<h3>{val}</h3>
+					<div className="crud">
+						<FaPencil
+							className="edit-icon"
+							onClick={() => {
+								editTask(val);
+							}}
+						/>
+						<FaTrash
+							className="delete-icon"
+							onClick={() => {
+								deleteTask(val);
+							}}
+						/>
+					</div>
+				</div>
+			</li>
+		);
+	});
+	return (
+		<div>
+			<ul className="todo-list-ul">{taskList}</ul>
+		</div>
+	);
+}
